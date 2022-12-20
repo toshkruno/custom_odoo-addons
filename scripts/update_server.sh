44bdd5703902654deba663955ec2ed213a9a6615
The-macharia <rcmacharia7@gmail.com>
2022-12-21 00:12:41 +0300

    [CHORE] add project based docker configs

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 set -e
-git pull origin odoo
-docker build -t odoo:15.0 .
+git pull origin main
+docker build -t tritel-index:15.0 .
 docker-compose down
 docker-compose up -d
 docker system prune -f
