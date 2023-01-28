31c097fcb2d1d7c4055727552cde70be8f7e4ec4
The-macharia <rcmacharia7@gmail.com>
2023-01-28 08:35:38 +0300

    [CHORE] remove unused module scs_freight

--- custom-addons/scs_freight/__init__.py
+++
@@ -1,6 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-"""Initialize models,wizard,report files."""
-
-from . import models
-from . import wizard
-from . import report
--- custom-addons/scs_freight/report/__init__.py
+++
@@ -1,4 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-
-from . import shipping_analysis
-from . import payment_receipt_report
