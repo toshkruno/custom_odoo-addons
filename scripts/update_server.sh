017dc7a32488585bf7cfb195c34023a1042070ba
Iam Odoo <d.korir@tritel.co.ke>
2023-01-31 20:50:17 +0000

    [CHORE] configure server upgrade scripts

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -2,7 +2,7 @@
 set -e
 git pull origin main
 docker build -t dennokorir/tritel-index:15.0 .
-docker-compose down
-docker-compose up -d
-docker push dennokorir/tritel-index:15.0
+docker compose down
+docker compose up -d
+#docker push dennokorir/tritel-index:15.0
 docker system prune -f
