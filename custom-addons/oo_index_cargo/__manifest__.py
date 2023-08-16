146d8ef50a061d39122941ffe9b7e060c7821689
The-macharia <rcmacharia7@gmail.com>
2023-08-16 09:43:38 +0300

    feat(app) add groups for vendor and contacts creation

--- custom-addons/oo_index_cargo/__manifest__.py
+++ custom-addons/oo_index_cargo/__manifest__.py
@@ -16,10 +16,11 @@
     'version': '15.0.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'hr_payroll', 'sale', 'freight_management_system'],
+    'depends': ['account', 'hr_payroll', 'sale'],
 
     # always loaded
     'data': [
+        'security/security.xml',
         'reports/invoice.xml',
         'reports/payslip.xml',
         'reports/sale_order.xml',
--- custom-addons/oo_index_cargo/models/models.py
+++ custom-addons/oo_index_cargo/models/models.py
@@ -3,8 +3,69 @@
 import uuid
 
 from odoo import api, models, fields
+from odoo.exceptions import UserError
 
 
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    @api.model
+    def create(self, vals):
+        if not self.env.user.has_group('oo_index_cargo.oo_contact_creation'):
+            raise UserError("You're not allowed to create contacts.")
+        return super().create(vals)
+
+    def write(self, vals):
+        if not self.env.user.has_group('oo_index_cargo.oo_contact_creation'):
+            raise UserError("You're not allowed to update contacts.")
+        return super().write(vals)
+
+    def unlink(self):
+        if not self.env.user.has_group('oo_index_cargo.oo_contact_creation'):
+            raise UserError("You're not allowed to delete contacts.")
+        return super().unlink()
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def create(self, vals):
+        if not self.env.user.has_group('oo_index_cargo.oo_product_creation'):
+            raise UserError("You're not allowed to create products.")
+        return super().create(vals)
+
+    def write(self, vals):
+        if not self.env.user.has_group('oo_index_cargo.oo_product_creation'):
+            raise UserError("You're not allowed to update products.")
+        return super().write(vals)
+
+    def unlink(self):
+        if not self.env.user.has_group('oo_index_cargo.oo_product_creation'):
+            raise UserError("You're not allowed to delete products.")
+        return super().unlink()
+    
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def create(self, vals):
+        if not self.env.user.has_group('oo_index_cargo.oo_product_creation'):
+            raise UserError("You're not allowed to create products.")
+        return super().create(vals)
+
+    def write(self, vals):
+        if not self.env.user.has_group('oo_index_cargo.oo_product_creation'):
+            raise UserError("You're not allowed to update products.")
+        return super().write(vals)
+
+    def unlink(self):
+        if not self.env.user.has_group('oo_index_cargo.oo_product_creation'):
+            raise UserError("You're not allowed to delete products.")
+        return super().unlink()
+    
+    
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
